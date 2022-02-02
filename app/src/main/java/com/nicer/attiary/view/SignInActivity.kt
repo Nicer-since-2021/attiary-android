13771bf2a3eb6999f175169fbe6305180cafb300
SJ-Lee33 <isjift55@ewhain.net>
2022-02-03 00:27:01 +0900

    feat: add kakao login func
    카카오 로그인 기능 추가
    log (Tag: "KakaotalkLogin")으로 accessToken 출력

---
+++ app/src/main/java/com/nicer/attiary/view/SignInActivity.kt
@@ -0,0 +1,80 @@
+package com.nicer.attiary.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import com.nicer.attiary.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+	val binding by lazy { ActivitySignInBinding.inflate(layoutInflater) }
+
+	// 카카오톡 로그인 불가해 카카오계정으로 로그인 시
+	val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+		if (error != null) {
+			Log.e("KakaoLogin", "카카오계정으로 로그인 실패", error)
+		} else if (token != null) {
+			Log.i("KakaoLogin", "카카오계정으로 로그인 성공 ${token.accessToken}")
+		}
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+
+		binding.btnKakaoSignin.setOnClickListener {
+			// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인력
+			if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+				UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+					if (error != null) {
+						Log.e("KakaotalkLogin", "카카오톡으로 로그인 실패", error)
+
+						// 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+						// 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+						if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+							return@loginWithKakaoTalk
+						}
+
+						// 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+						UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+					} else if (token != null) {
+						Log.i("KakaotalkLogin", "카카오톡으로 로그인 성공 ${token.accessToken}")
+					}
+				}
+			} else {
+				UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+			}
+		}
+
+//		binding.btnKakaoSignin.setOnClickListener {
+//			UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
+//				if (error != null) {
+//					Log.e("KakaoLogin", "로그인 실패", error)
+//				} else if (token != null) {
+//					Log.i("KakaoLogin", "로그인 성공 ${token.accessToken}")
+//
+//					/*
+//					// 사용자 요청 정보
+//					UserApiClient.instance.me { user, error ->
+//						if (error != null) {
+//							Log.e("카카오로그인", "사용자 정보 요청 실패", error)
+//						} else if (user != null) {
+//							Log.i(
+//								"카카오로그인", "사용자 정보 요청 성공" +
+//										"\n회원번호: ${user.id}" +
+//										"\n이메일: ${user.kakaoAccount?.email}" +
+//										"\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+//										"\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+//							)
+//						}
+//					}
+//					*/
+//				}
+//			}
+//		}
+	}
+}
