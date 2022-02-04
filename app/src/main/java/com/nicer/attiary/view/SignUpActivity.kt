5794acfd2d911c57406e203963656d0af1aa0796
Jueun <jueun000101@gmail.com>
2022-02-04 22:04:56 +0900

    fix: change findViewById to view binding
    에러를 고친 핵심 코드는 `setContentView(binding.root)` 이 부분입니다.

--- app/src/main/java/com/nicer/attiary/view/SignUpActivity.kt
+++ app/src/main/java/com/nicer/attiary/view/SignUpActivity.kt
@@ -2,8 +2,8 @@ package com.nicer.attiary.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.nicer.attiary.R
@@ -18,60 +18,21 @@ class SignUpActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_sign_up)
+		setContentView(binding.root)
 
 		helper = Room.databaseBuilder(this, UserHelper::class.java, "user")
 			.allowMainThreadQueries()
 			.build()
 
-		/**
-		 * convert findViewById to viewbinding
-		 */
-		val buttonBack = findViewById<ImageButton>(R.id.buttonBack)
-		buttonBack.setOnClickListener {
+		binding.buttonBack.setOnClickListener {
 			finish()
 		}
-/*
-		binding.backButton.setOnClickListener {
-			finish()
-		}
-*/
 
-
-		/**
-		 * convert findViewById to viewbinding
-		 */
 		val intent = Intent(this, SettingPasswordActivity::class.java)
-		val buttonSettingPassword = findViewById<Button>(R.id.buttonSettingPassword)
-		buttonSettingPassword.setOnClickListener {
-			startActivity(intent)
-		}
-/*
 		binding.buttonSettingPassword.setOnClickListener {
 			startActivity(intent)
 		}
-*/
 
-
-		/**
-		 * convert findViewById to viewbinding
-		 */
-		val spinnerMonth = findViewById<Spinner>(R.id.spinnerMonth)
-		ArrayAdapter.createFromResource(
-			this, R.array.months_array, android.R.layout.simple_spinner_item
-		).also { adapter ->
-			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-			spinnerMonth.adapter = adapter
-		}
-
-		val spinnerDay = findViewById<Spinner>(R.id.spinnerDay)
-		ArrayAdapter.createFromResource(
-			this, R.array.days_array, android.R.layout.simple_spinner_item
-		).also { adapter ->
-			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-			spinnerDay.adapter = adapter
-		}
-/*
 		ArrayAdapter.createFromResource(
 			this, R.array.months_array, android.R.layout.simple_spinner_item
 		).also { adapter ->
@@ -85,33 +46,26 @@ class SignUpActivity : AppCompatActivity() {
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 			binding.spinnerDay.adapter = adapter
 		}
-*/
 
-		/**
-		 * convert findViewById to viewbinding
-		 */
-		val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
-		val e = findViewById<EditText>(R.id.editEmail)
-		val n = findViewById<EditText>(R.id.editName)
-		buttonSubmit.setOnClickListener {
-			val user =
-				User.Builder(e.text.toString(), n.text.toString())
-					.birthdayMonth(spinnerMonth.selectedItem.toString().toInt())
-					.birthdayDay(spinnerDay.selectedItem.toString().toInt())
-					.build()
-			Log.d("user info", user.toString())
-			helper?.userDao()?.insert(user)
-		}
-/*
 		binding.buttonSubmit.setOnClickListener {
-//			TODO, @jueun: parameter validation
-			val user =
-				User.Builder(binding.editEmail.text.toString(), binding.editName.text.toString())
-					.birthdayMonth(binding.spinnerMonth.selectedItem.toString().toInt())
-					.birthdayDay(binding.spinnerDay.selectedItem.toString().toInt())
-					.build()
-			helper?.userDao()?.insert(user)
+			val email = binding.editEmail.text.toString()
+			val name = binding.editName.text.toString()
+			val user = saveUser(email, name)
+			Toast.makeText(this, "$user 저장됨!", Toast.LENGTH_SHORT).show()
 		}
-*/
+	}
+
+	private fun saveUser(email: String, name: String): User {
+		val user =
+			User.Builder(email, name)
+				.birthdayMonth(binding.spinnerMonth.selectedItem.toString().toInt())
+				.birthdayDay(binding.spinnerDay.selectedItem.toString().toInt())
+				.build()
+		helper?.userDao()?.insert(user)
+		return user
+	}
+
+	private fun isValidEmail(email: String): Boolean {
+		return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 	}
 }
