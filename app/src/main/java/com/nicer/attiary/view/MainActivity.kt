c6bdbf0675316aa39deed39dfa16f67a4e258c5b
SJ-Lee33 <isjift55@ewhain.net>
2022-02-08 21:14:18 +0900

    feat: add playing sig bgm func
    MainActivity에서 홈버튼 누르면 일시정지, 앱으로 돌아오면 다시 재생, intent 이동시에도 노래 계속 재생

--- app/src/main/java/com/nicer/attiary/view/MainActivity.kt
+++ app/src/main/java/com/nicer/attiary/view/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.nicer.attiary.view
 
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NO_USER_ACTION
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.nicer.attiary.R
 import com.nicer.attiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 	private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+	//private lateinit var signiture_bgm: MediaPlayer
+	private var signiture_bgm: MediaPlayer? = null
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
+
+		signiture_bgm = MediaPlayer.create(this, R.raw.bluedream)
+		signiture_bgm?.setLooping(true)
+		signiture_bgm?.start()
 
 		binding.btnSay.setOnClickListener {
 			binding.textSay.text = "Hello, Kotlin!"
@@ -19,12 +28,28 @@ class MainActivity : AppCompatActivity() {
 
 		val intent = Intent(this, SignUpActivity::class.java)
 		binding.btnStart.setOnClickListener {
+			intent.addFlags(FLAG_ACTIVITY_NO_USER_ACTION)
 			startActivity(intent)
 		}
 
 		val intent_signin = Intent(this, SignInActivity::class.java)
 		binding.btnGotoSignin.setOnClickListener {
+			intent_signin.addFlags(FLAG_ACTIVITY_NO_USER_ACTION)
 			startActivity(intent_signin)
 		}
+
+		binding.btnPlay.setOnClickListener {
+			signiture_bgm?.start()
+		}
+	}
+
+	override fun onUserLeaveHint() {
+		super.onUserLeaveHint()
+		signiture_bgm?.pause()
+	}
+
+	override fun onResume() {
+		super.onResume()
+		signiture_bgm?.start()
 	}
 }
