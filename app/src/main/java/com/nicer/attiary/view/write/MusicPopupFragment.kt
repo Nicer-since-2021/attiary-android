036cc65b285345d006c5b5c5c01a61707d5b8546
SJ-Lee33 <isjift55@ewhain.net>
2022-02-17 18:22:34 +0900

    feat: show music popup
    mainactivity 상단의 음표 버튼을 꾹 누르면 음악 팝업이 띄워지도록 함

---
+++ app/src/main/java/com/nicer/attiary/view/write/MusicPopupFragment.kt
@@ -0,0 +1,20 @@
+package com.nicer.attiary.view.write
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nicer.attiary.databinding.FragmentMusicPopupBinding
+
+class MusicPopupFragment : Fragment() {
+	lateinit var binding: FragmentMusicPopupBinding
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		binding = FragmentMusicPopupBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+}
