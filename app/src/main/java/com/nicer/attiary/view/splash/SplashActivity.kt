ae6422914e840147541c4d9b5cd50df4717a2119
SJ-Lee33 <isjift55@ewhain.net>
2022-03-18 19:39:36 +0900

    remove: remove SignInActivity
    - SignInActivity 삭제
    - Splash 이후 바로 SignUpActivity로 이동하도록 수정

--- app/src/main/java/com/nicer/attiary/view/splash/SplashActivity.kt
+++ app/src/main/java/com/nicer/attiary/view/splash/SplashActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.nicer.attiary.R
-import com.nicer.attiary.view.auth.sign_in.SignInActivity
+import com.nicer.attiary.view.auth.sign_up.SignUpActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -14,10 +14,8 @@ class SplashActivity : AppCompatActivity() {
 		setContentView(R.layout.activity_splash)
 		Handler().postDelayed({
 //			startActivity(Intent(this, SettingPasswordActivity::class.java))
-			startActivity(Intent(this, SignInActivity::class.java))
+			startActivity(Intent(this, SignUpActivity::class.java))
 			finish()
 		}, 2000)
 	}
-
-
 }
