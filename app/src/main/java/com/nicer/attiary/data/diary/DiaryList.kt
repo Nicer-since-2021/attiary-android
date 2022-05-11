adcd45f38a047b7e7d8dbcb39f658c43677bc5b9
S00MIN-KIM <soomin0219@ewhain.net>
2022-05-11 20:12:30 +0900

    feat: add calendar decorator by emotion

--- app/src/main/java/com/nicer/attiary/data/diary/DiaryList.kt
+++ app/src/main/java/com/nicer/attiary/data/diary/DiaryList.kt
@@ -1,11 +1,15 @@
 package com.nicer.attiary.data.diary
 
 import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.prolificinteractive.materialcalendarview.CalendarDay
+
 
 class DiaryList(context: Context) {
 	private var sharedPref = context.getSharedPreferences("Diary", Context.MODE_PRIVATE)
 	// 목록에 일기 추가
-	fun addDiary(rDate : Long, emotion : String) {
+	fun addDiary(rDate : CalendarDay, emotion : String) {
 		sharedPref.edit().apply {
 			putString(rDate.toString(), emotion)
 			apply()
@@ -13,20 +17,74 @@ class DiaryList(context: Context) {
 	}
 
 	// 목록에서 해당 날짜 일기 제거
-	fun removeDiary(rDate : Long) {
+	fun removeDiary(rDate : CalendarDay) {
 		sharedPref.edit().apply {
 			remove(rDate.toString())
 			apply()
 		}
 	}
 
-	// 해당 날짜 일기가 있는가?
-	fun isExist(rDate : Long): Boolean {
+	fun isExist(rDate : CalendarDay?): Boolean {
 		if (sharedPref.contains(rDate.toString())) {
 			return true
 		}
 		return false
 	}
 
+	fun isAnger(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "anger") {
+			return true
+		}
+		return false
+	}
+
+	fun isAnxiety(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "anxiety") {
+			return true
+		}
+		return false
+	}
+
+	fun isHope(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "hope") {
+			return true
+		}
+		return false
+	}
+
+	fun isJoy(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "joy") {
+			return true
+		}
+		return false
+	}
+
+	fun isNeutrality(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "neutrality") {
+			return true
+		}
+		return false
+	}
+
+	fun isRegret(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "regret") {
+			return true
+		}
+		return false
+	}
+
+	fun isSadness(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "sadness") {
+			return true
+		}
+		return false
+	}
+
+	fun isTiredness(rDate : CalendarDay?): Boolean {
+		if (sharedPref.getString(rDate.toString(), "") == "tiredness") {
+			return true
+		}
+		return false
+	}
 
 }
