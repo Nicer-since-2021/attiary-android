e33eb82a9b41fea44f722271ceb6c876fb05b359
hoit1302 <jueun000101@gmail.com>
2022-05-12 22:02:07 +0900

    feat: add Report Builder, queries

--- app/src/main/java/com/nicer/attiary/data/report/ReportDao.kt
+++ app/src/main/java/com/nicer/attiary/data/report/ReportDao.kt
@@ -8,9 +8,15 @@ interface ReportDao {
 	@Insert(onConflict = REPLACE)
 	fun insert(report: Report)
 
-	@Query("select * from report where date = :rDate")
-	fun findByDate(rDate: Long): Report
+	@Query("select * from report where date = :rDate ORDER BY report_id DESC")
+	fun findByDate(rDate: String): Report
 
 	@Query("delete from report where date = :rDate")
-	fun delete(rDate: Long)
+	fun delete(rDate: String)
+
+	@Query("SELECT * FROM report ORDER BY date DESC LIMIT :rowCount")
+	fun findTop10(rowCount: Int) : MutableList<Report>
+
+	@Query("SELECT * FROM report ORDER BY date DESC, happiness DESC LIMIT 3")
+	fun findHappinessTop3(): MutableList<Report>
 }
