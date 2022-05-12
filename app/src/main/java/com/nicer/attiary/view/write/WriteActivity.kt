d4c971f81e420e864f9ae73e4bb92e3a5127a211
SJ-Lee33 <isjift55@ewhain.net>
2022-05-12 19:55:56 +0900

    fix: modify timing of ON/OFF bgm on the loading screen
    일기 저장 버튼을 누르고, 로딩화면이 나올 때 emo music은 끄고 sig music을 켜도록 수정함

--- app/src/main/java/com/nicer/attiary/view/write/WriteActivity.kt
+++ app/src/main/java/com/nicer/attiary/view/write/WriteActivity.kt
@@ -76,9 +76,8 @@ class WriteActivity : AppCompatActivity() {
         }
 
         binding.saveBtn.setOnClickListener {
-            startService(sigmu_intent)
-
             hideKeyboard()
+            
             if (binding.contextEditText.text.isBlank()) {
                 val builder = AlertDialog.Builder(this)
                 builder.setMessage("내용을 입력하세요.")
@@ -89,136 +88,11 @@ class WriteActivity : AppCompatActivity() {
                     DiaryList(this).removeDiary(CalendarDay(year, month, dayOfMonth))
                 }
 
-                //로딩화면
-
-                val content = binding.contextEditText.text.toString()
-                var emotions = hashMapOf<String, Int>()
-                var dDepression = 0
-                RetrofitObject.getApiService().getReport(content)
-                    .enqueue(object : Callback<Classification> {
-                        override fun onResponse(
-                            call: Call<Classification>,
-                            response: Response<Classification>
-                        ) {
-                            Log.d("YMC", "ㅎ")
-                            if (response.isSuccessful) {
-                                var result: Classification? = response.body()
-                                Log.d("YMC", "onResponse 성공: ")
-                                emotions.put("anger", ((result?.anger)?.times(100))?.toInt()!!)
-                                emotions.put("anxiety", ((result.anxiety)?.times(100))?.toInt()!!)
-                                emotions.put("hope", ((result.hope)?.times(100))?.toInt()!!)
-                                emotions.put("joy", ((result.joy)?.times(100))?.toInt()!!)
-                                emotions.put("regret", ((result.regret)?.times(100))?.toInt()!!)
-                                emotions.put("sadness", ((result.sadness)?.times(100))?.toInt()!!)
-                                emotions.put(
-                                    "tiredness",
-                                    ((result.tiredness)?.times(100))?.toInt()!!
-                                )
-                                dDepression = (result.depression)?.times(100)?.toInt()!!
-                                Log.d("result", result.toString())
-                            } else {
-                                // 통신 실패
-                                Log.d("YMC", "onResponse 실패")
-                            }
-                        }
-
-                        override fun onFailure(call: Call<Classification>, t: Throwable) {
-                            // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
-                            Log.d("YMC", "onFailure 에러: " + t.message.toString())
-                        }
-                    })
-
-                Handler(Looper.getMainLooper()).postDelayed({
-                    try {
-                        Log.d("emotions", emotions.toString())
-                        val emotions2 = emotions.toList().sortedByDescending { (_, value) -> value }
-                            .toMap() as HashMap<String, Int>
-                        val e1 = emotions2.keys.elementAt(0)
-                        val p1 = emotions2.getValue(e1)
-                        var happiness = emotions.get("joy")?.plus(emotions.get("hope")!!)
-                        var depression = emotions.get("anger")?.plus(emotions.get("sadness")!!)
-                            ?.plus(emotions.get("anxiety")!!)?.plus(emotions.get("tiredness")!!)
-                            ?.plus(emotions.get("regret")!!)?.plus(dDepression)
-                        var representative = setRepresentative(e1, p1)
-                        CoroutineScope(Dispatchers.IO).launch {
-                            database?.ReportDao()?.insert(
-                                Report(
-                                    rDate,
-                                    content,
-                                    representative,
-                                    emotions,
-                                    happiness!!,
-                                    depression!!,
-                                    ""
-                                )
-                            )
-                        }
-                        if (p1 == 0)
-                            DiaryList(this).addDiary(
-                                CalendarDay(year, month, dayOfMonth),
-                                "neurality"
-                            )
-                        else
-                            DiaryList(this).addDiary(CalendarDay(year, month, dayOfMonth), e1)
-                    } catch (e: ClassCastException) {
-                        Log.d("[error]", "ClassCastException")
-                        //로딩화면 닫고
-                        //서버가 불안정하니 다음에 다시 시도하라는 창: 일기내용만 저장해둘게요! 분석을 다시 시도하려면 수정 버튼을 누르고 다시 저장해보세요!
-                        emotions.put("anger", 0)
-                        emotions.put("anxiety", 0)
-                        emotions.put("hope", 0)
-                        emotions.put("joy", 0)
-                        emotions.put("regret", 0)
-                        emotions.put("sadness", 0)
-                        emotions.put("tiredness", 0)
-
-                        var happiness = emotions.get("joy")?.plus(emotions.get("hope")!!)
-                        var depression = emotions.get("anger")?.plus(emotions.get("sadness")!!)
-                            ?.plus(emotions.get("anxiety")!!)?.plus(emotions.get("tiredness")!!)
-                            ?.plus(emotions.get("regret")!!)?.plus(dDepression)
-                        var representative = "neutrality"
-                        CoroutineScope(Dispatchers.IO).launch {
-                            database?.ReportDao()?.insert(
-                                Report(
-                                    rDate,
-                                    content,
-                                    representative,
-                                    emotions,
-                                    happiness!!,
-                                    depression!!,
-                                    ""
-                                )
-                            )
-                        }
-                        DiaryList(this).addDiary(CalendarDay(year, month, dayOfMonth), "neutrality")
-                    } finally {
-                        //로딩화면 닫힘
-                        val intent = Intent(this, DiaryActivity::class.java)
-                        intent.putExtra("year", year)
-                        intent.putExtra("month", month)
-                        intent.putExtra("dayOfMonth", dayOfMonth)
-                        startActivity(intent)
-                        finish()
-                    }
-                }, 10000)
-
-
-            }
-        }
-
-        binding.saveBtn.setOnClickListener {
-            startService(sigmu_intent)
-
-            hideKeyboard()
-            if (binding.contextEditText.text.isBlank()) {
-                val builder = AlertDialog.Builder(this)
-                builder.setMessage("내용을 입력하세요.")
-                builder.setPositiveButton("확인", null)
-                builder.show()
-            } else {
-                if (str != "null") {
-                    DiaryList(this).removeDiary(CalendarDay(year, month, dayOfMonth))
+                if (emoMP?.isPlaying == true) {
+                    emoMP?.stop()
+                    emoMP?.release()
                 }
+                startService(sigmu_intent)
 
                 binding.wholeView.bringToFront()
                 setLoadingFrag()
