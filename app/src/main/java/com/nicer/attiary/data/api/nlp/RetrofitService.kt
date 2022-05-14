15f839e689daf5c281794cbdb230c943b462b83d
hoit1302 <jueun000101@gmail.com>
2022-05-14 15:20:35 +0900

    Merge branch 'main' of github.com:Nicer-since-2021/ATTIARY-android into main
    # Conflicts:
    #	app/src/main/java/com/nicer/attiary/view/write/WriteActivity.kt

--- app/src/main/java/com/nicer/attiary/data/api/nlp/RetrofitService.kt
+++ app/src/main/java/com/nicer/attiary/data/api/nlp/RetrofitService.kt
@@ -2,22 +2,24 @@ package com.nicer.attiary.data.api.nlp
 
 import com.nicer.attiary.data.api.nlp.chat.Chat
 import com.nicer.attiary.data.api.nlp.classification.Classification
+import com.nicer.attiary.data.api.nlp.classification.ShortClassification
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RetrofitService {
-//    @GET("chatbot/v2/")
-//    fun getUser(): Call<User>
-
     @GET("chatbot/b")
     fun getChatRes(
         @Query("s") s: String
     ): Call<Chat>
 
+    @GET("emotion")
+    fun getChatEmo(
+        @Query("s") s: String
+    ): Call<ShortClassification>
+
     @GET("diary")
     fun getReport(
         @Query("s") s: String
-    ) : Call<Classification>
-
+    ): Call<Classification>
 }
