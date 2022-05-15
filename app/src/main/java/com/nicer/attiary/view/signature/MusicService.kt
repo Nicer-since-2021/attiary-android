d0b2fec2b3c736304d67aabb1d4706f929e63b81
SJ-Lee33 <isjift55@ewhain.net>
2022-05-15 19:49:24 +0900

    feat: synchronize picker with current emotion
    음악 선택 프래그먼트의 피커와 현재의 감정을 동기화함

--- app/src/main/java/com/nicer/attiary/view/signature/MusicService.kt
+++ app/src/main/java/com/nicer/attiary/view/signature/MusicService.kt
@@ -10,29 +10,31 @@ import com.nicer.attiary.view.common.GlobalApplication
 
 
 class MusicService : Service() {
-	var mp: MediaPlayer? = null
+    var mp: MediaPlayer? = null
 
-	@Nullable
-	override fun onBind(intent: Intent?): IBinder? {
-		return null
-	}
+    @Nullable
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 
-	override fun onCreate() {
-		super.onCreate()
-		if (mp == null) mp = MediaPlayer.create(this, R.raw.bluedream)
-		mp?.isLooping = true
-	}
+    override fun onCreate() {
+        super.onCreate()
+        if (mp == null) mp = MediaPlayer.create(this, R.raw.bluedream)
+        mp?.isLooping = true
+    }
 
-	override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-		var sigCheck = GlobalApplication.musicPrefs.getString("sigMusic","")
-		if (sigCheck == "sON"){
-			mp?.start()
-		}
-		return super.onStartCommand(intent, flags, startId)
-	}
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        var sigCheck = GlobalApplication.musicPrefs.getString("sigMusic", "")
+        if (sigCheck == "sON") {
+            mp?.start()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
 
-	override fun onDestroy() {
-		mp?.stop()
-		super.onDestroy()
-	}
+    override fun onDestroy() {
+        mp?.stop()
+        mp?.release()
+        mp = null
+        super.onDestroy()
+    }
 }
--- app/src/main/java/com/nicer/attiary/view/write/MusicPopupFragment.kt
+++ app/src/main/java/com/nicer/attiary/view/write/MusicPopupFragment.kt
@@ -6,34 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.nicer.attiary.databinding.FragmentMusicPopupBinding
 
 class MusicPopupFragment : Fragment() {
-	lateinit var binding: FragmentMusicPopupBinding
-	private val viewModel: MusicViewModel by activityViewModels()
+    lateinit var binding: FragmentMusicPopupBinding
+    private val viewModel: MusicViewModel by activityViewModels()
 
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-		binding = FragmentMusicPopupBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMusicPopupBinding.inflate(inflater, container, false)
 
-		SetPicker()
+        SetPicker()
 
-		return binding.root
-	}
+        viewModel.getEmotion.observe(viewLifecycleOwner, Observer { item ->
+            binding.numberPicker.value = item
+        })
+        return binding.root
+    }
 
-	private fun SetPicker() {
-		val picker = binding.numberPicker
-		val bgm_category = arrayOf("기쁨", "희망", "슬픔", "분노", "불안", "피곤", "후회")
-		picker.minValue = 0
-		picker.maxValue = bgm_category.size - 1
-		picker.displayedValues = bgm_category
-		picker.wrapSelectorWheel = true // 순환
+    private fun SetPicker() {
+        val picker = binding.numberPicker
+        val bgm_category = arrayOf("기쁨", "희망", "중립", "분노", "슬픔", "불안", "피곤", "후회")
+        picker.minValue = 0
+        picker.maxValue = bgm_category.size - 1
+        picker.displayedValues = bgm_category
+        picker.wrapSelectorWheel = true // 순환
 
-		// 변화값 감지
-		picker.setOnValueChangedListener { _, _, newVal ->
-			viewModel.setEmotion(newVal)
-		}
-	}
+        // 변화값 감지
+        picker.setOnValueChangedListener { _, _, newVal ->
+            viewModel.setEmotion(newVal)
+        }
+    }
 }
